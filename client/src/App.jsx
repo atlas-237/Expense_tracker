e16a0f4c429e1791b2e12a699a29c8bd14ef5190
atlas-237 <juniorwouagoum@gmail.com>
2023-03-31 07:13:23 +0100

    label component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Graph from "./components/Graph";
 
 const App = () => {
   return (
@@ -8,6 +9,13 @@ const App = () => {
         <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">
           Expense Tracker
         </h1>
+
+        {/* grid columuns */}
+        <div className="grid md:grid-cols-2 gap-4">
+          {/* Chart */}
+          <Graph></Graph>
+          {/* Form */}
+        </div>
       </div>
     </div>
   );
---
+++ client/src/components/Labels.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const LabelComponent = () => {
+  return (
+    <div className="labels flex justify-between">
+      <div className="flex gap-2">
+        <div
+          className="w-2 h-2 rounded py-3"
+          style={{ background: "#f9c74f" }}
+        ></div>
+        <h3 className="text-md">Savings</h3>
+      </div>
+      <h3 className="font-bold">45%</h3>
+    </div>
+  );
+};
+
+const Labels = () => {
+  return <>{LabelComponent()}</>;
+};
+
+export default Labels;
